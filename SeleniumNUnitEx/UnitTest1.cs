677bd8d4cfb0bc9326cff5afa2738093aea7789f
franklinjisno29 <Franklin.Jisno@ust.com>
2023-11-10 15:00:23 +0530

    Add project files.

---
+++ SeleniumNUnitEx/UnitTest1.cs
@@ -0,0 +1,31 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumNUnitEx
+{
+    public class Tests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+           driver = new ChromeDriver();
+
+            driver.Url = "https://www.google.com";
+        }
+
+        [Test]
+        public void CheckForTitle()
+        {
+            Thread.Sleep(2000);
+            string title = driver.Title;
+            Assert.AreEqual("Google", title);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
